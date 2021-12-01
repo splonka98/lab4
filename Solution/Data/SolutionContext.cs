c558d423e0af6a052905241a09ddde3afcdb7d34
splonka98 <szymon.plonka@microsoft.wsei.edu.pl>
2021-12-01 20:02:43 +0100

    pls
    .

---
+++ Solution/Data/SolutionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Solution.Models;
+
+namespace Solution.Data
+{
+    public class SolutionContext : DbContext
+    {
+        public SolutionContext (DbContextOptions<SolutionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Solution.Models.Movie> Movie { get; set; }
+    }
+}
